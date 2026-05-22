ce8fe1a15521b99b0a17bfa7678efcaff315daff
danielson <danielsonhong@gmail.com>
2026-05-22 22:36:59 +0700

    feat: add Cards widget with customizable spacing and styling

---
+++ jalan_aman/lib/components/card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:jalan_aman/theme/theme.dart';
+
+enum Spacing { xs, sm, md, base, lg, xl, xxl, xxxl }
+
+class Cards extends StatelessWidget {
+  const Cards({super.key, this.appSpacing = Spacing.xl, this.child});
+
+  final Spacing appSpacing;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(_appSpacing),
+      decoration: BoxDecoration(
+        color: AppColors.surface,
+        borderRadius: AppRadius.cardRadius,
+        boxShadow: AppShadows.card,
+      ),
+      child: child,
+    );
+  }
+
+  double get _appSpacing {
+    switch (appSpacing) {
+      case Spacing.xs:
+        return AppSpacing.xs;
+      case Spacing.sm:
+        return AppSpacing.sm;
+      case Spacing.md:
+        return AppSpacing.md;
+      case Spacing.base:
+        return AppSpacing.base;
+      case Spacing.lg:
+        return AppSpacing.lg;
+      case Spacing.xl:
+        return AppSpacing.xl;
+      case Spacing.xxl:
+        return AppSpacing.xxl;
+      case Spacing.xxxl:
+        return AppSpacing.xxxl;
+    }
+  }
+}
