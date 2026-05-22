a13feb27d3dadf1cd474ed7327ff6c27e3eea302
danielson <danielsonhong@gmail.com>
2026-05-22 22:37:21 +0700

    feat: implement HomePage with bottom navigation and page routing

--- jalan_aman/lib/pages/home_page.dart
+++ jalan_aman/lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:jalan_aman/pages/map_page.dart';
+import 'package:jalan_aman/pages/profile_page.dart';
+import 'package:jalan_aman/pages/report_page.dart';
+import 'package:jalan_aman/theme/theme.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +12,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentIndex = 1;
+
+  final List<Widget> _pages = const [ReportPage(), MapPage(), ProfilePage()];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: IndexedStack(index: currentIndex, children: _pages),
+      bottomNavigationBar: _BottomNav(
+        currentIndex: currentIndex,
+        onTap: (value) => setState(() {
+          currentIndex = value;
+        }),
+      ),
+    );
+  }
+}
+
+class _BottomNav extends StatelessWidget {
+  const _BottomNav({required this.currentIndex, required this.onTap});
+
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      selectedIndex: currentIndex,
+      onDestinationSelected: onTap,
+      backgroundColor: AppColors.surface,
+      indicatorColor: AppColors.primary,
+      surfaceTintColor: Colors.transparent,
+      elevation: 0,
+      shadowColor: AppColors.shadowColor,
+      labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+      destinations: const [
+        NavigationDestination(
+          icon: Icon(Icons.list_alt_outlined),
+          selectedIcon: Icon(Icons.list_alt_rounded, color: Colors.white),
+          label: 'Laporan',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.map_outlined),
+          selectedIcon: Icon(Icons.map_rounded, color: Colors.white),
+          label: "Map",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.person_outline_rounded),
+          selectedIcon: Icon(Icons.person_rounded, color: Colors.white),
+          label: 'Profil',
+        ),
+      ],
+    );
   }
 }
